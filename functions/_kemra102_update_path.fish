686e2b940bbb56fa71c98049a0d49b4ed1b06985
Danny Roberts <danny@thefallenphoenix.net>
2020-12-27 13:44:42 +0000

    Fix if statement in update_path function.

--- functions/_kemra102_update_path.fish
+++ functions/_kemra102_update_path.fish
@@ -1,5 +1,5 @@
 function _kemra102_update_path -a path -d 'Check if a filepath exists & if so adds it to $PATH.'
-    if not contains $path $PATH and [ -d $path ]
-        set PATH $PATH $path
+    if not contains $path $PATH; test -d $path; status --is-login
+        set -gxp PATH $path
     end
 end
