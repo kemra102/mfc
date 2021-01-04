5c6af134f4b1007d5937bf9011958957af4b651c
Danny Roberts <danny@thefallenphoenix.net>
2021-01-04 10:18:19 +0000

    Add aws-vault backend var.

--- conf.d/_kemra102_aws.fish
+++ conf.d/_kemra102_aws.fish
@@ -2,3 +2,6 @@ set AWS_DEFAULT_REGION eu-west-1
 
 # Set the AWS Pager to '' so one isn't used by default
 set AWS_PAGER ''
+
+# Technically for aws-vault not AWS itself
+set -x AWS_VAULT_BACKEND kdewallet
