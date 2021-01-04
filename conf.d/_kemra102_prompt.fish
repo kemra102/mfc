d183c6b4a0b2cef3730e860e6d5d35ab2b2f90dd
Danny Roberts <danny@thefallenphoenix.net>
2021-01-04 13:37:37 +0000

    Export prompt vars.

--- conf.d/_kemra102_prompt.fish
+++ conf.d/_kemra102_prompt.fish
@@ -1,4 +1,4 @@
 # Configure options on my chosen prompt theme
-set theme_display_aws_vault_profile yes
-set theme_display_date no
-set theme_display_git_default_branch yes
+set -x theme_display_aws_vault_profile yes
+set -x theme_display_date no
+set -x theme_display_git_default_branch yes
